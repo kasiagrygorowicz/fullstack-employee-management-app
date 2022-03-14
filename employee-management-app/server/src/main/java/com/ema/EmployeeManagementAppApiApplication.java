2a5512c63adb5a754d9b840f3afe7986d7fb3712
kasiagrygorowicz <kasiagrygorowicz@interia.eu>
2022-03-14 17:00:15 +0100

    initial commit

---
+++ employee-management-app/server/src/main/java/com/ema/EmployeeManagementAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.ema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementAppApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementAppApiApplication.class, args);
+    }
+
+}
---
+++ employee-management-app/server/src/test/java/com/ema/EmployeeManagementAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementAppApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
